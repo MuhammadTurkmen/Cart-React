27fb3082a935e0e1f9de9f9aaec7cf2941cc419c
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-18 10:38:15 +0430

    init

--- src/CartItem.js
+++ src/CartItem.js
@@ -27,7 +27,7 @@ const CartItem = ({ id, img, title, price, amount, toggleAmount }) => {
         {/* amount */}
         <p className='amount'>{amount}</p>
         {/* decrease amount */}
-        <button className='amount-btn' onClick={() => toggleAmount(id)}>
+        <button className='amount-btn' onClick={() => toggleAmount(id, 'dec')}>
           <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
             <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
           </svg>
