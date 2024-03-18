9dd2d7ed07a61a10f3ce0c01bfe0b16a75b7af6a
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-18 09:27:55 +0430

    init

--- src/reducer.js
+++ src/reducer.js
@@ -17,6 +17,17 @@ const reducer = (state, action) => {
         return {...state, cart: tepmCart}
     }
 
+    if(action.type === 'DECREASE') {
+        let tepmCart = state.cart.map((cartItem) => {
+            if(cartItem.id === action.payload) {
+                return {...cartItem, amount: cartItem.amount + 1}
+            }
+            return cartItem
+        })
+        return {...state, cart: tepmCart}
+    }
+
+
     return state
 }
 
