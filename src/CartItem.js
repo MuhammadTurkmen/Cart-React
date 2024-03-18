bf0037a13861f50892715478734654db10210fe5
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-18 09:20:43 +0430

    init

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useGlobalContext } from './context'
 const CartItem = ({ id, img, title, price, amount }) => {
-  const {remove} = useGlobalContext()
+  const {remove, increase, decrease} = useGlobalContext()
   
   return (
     <article className='cart-item'>
