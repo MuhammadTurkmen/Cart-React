e2393406fe7f37b930533dec996b84b17cb4c9eb
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-18 10:29:26 +0430

    init

--- src/reducer.js
+++ src/reducer.js
@@ -52,7 +52,7 @@ const reducer = (state, action) => {
     }
     
     if(action.type === 'DISPLAY_ITEMS') {
-        return {...state, cart:action.payload}
+        return {...state, cart:action.payload, loading:false}
     }
 
     return state
