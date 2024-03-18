e7dc281223344ebb0ed986ecc5151bd40576d123
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-18 10:20:21 +0430

    init

--- src/context.js
+++ src/context.js
@@ -37,7 +37,7 @@ const AppProvider = ({ children }) => {
     dispatch({type: 'LOADING'})
     const response = await fetch(url)
     const cart = await response.json()
-    dispatch({type: 'DISPLAY_ITEMS'})
+    dispatch({type: 'DISPLAY_ITEMS', payload: cart})
   }
   
   useEffect(() => {
